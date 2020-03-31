ff08debdd558b9457ee90c2c968ac5f6c31cf52f
wnz <wnz_99@hust.edu.cn>
2020-03-31 16:58:30 +0800

    ugrade cli play player can input init grid

--- minesweep_cli.py
+++ minesweep_cli.py
@@ -2,19 +2,30 @@ from config import SIZE_X, SIZE_Y
 from Mine_Sweep import Mine_Map
 
 if __name__ == "__main__":
-    MINES = Mine_Map(2, 2)
-    MINES.Print_Mines()
 
-    MINES.Disp_All()
-    MINES.Disp_UI()
-    while MINES.Status == '1':
-        x_in = input('x=')
-        y_in = input('y=')
-        do = input('do=')
-        x = int(x_in)
-        y = int(y_in)
-        if do == '1':
-            MINES.Click(x, y)
-        elif do == '2':
-            MINES.Flag(x, y)
-        MINES.Disp_UI()
+    Steps = 0
+    Continue_Game = True
+    while Continue_Game:
+
+        if Steps == 0:
+            print('Game Start!')
+            print('Map Size (%d,%d)' % (SIZE_X, SIZE_Y))
+            print('init your game')
+            x = int(input('x='))
+            y = int(input('y='))
+
+            Mine = Mine_Map(x, y)
+            Mine.Print_Mines()
+        else:
+            x = int(input('x='))
+            y = int(input('y='))
+            do = input('1 is click, 2 is flag \t')
+            if do == '1':
+                Mine.Click(x, y)
+            elif do == '2':
+                Mine.Flag(x, y)
+
+        Mine.Disp_UI()
+        Steps += 1
+        if Mine.Status != '1':
+            Continue_Game = False
