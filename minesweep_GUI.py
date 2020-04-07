28d5cd9d94fffabd955cd808f07346d800bf8b1a
wnz <wnz_99@hust.edu.cn>
2020-04-07 18:01:12 +0800

    GUI play

---
+++ minesweep_GUI.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+from Mine_GUI import Mine_GUI
+
+SIZE_X = 12
+SIZE_Y = 12
+MINE_NUM = 15
+
+if __name__ == '__main__':
+    windows = tk.Tk()
+    minesweep_GUI = Mine_GUI(windows, SIZE_X, SIZE_Y, MINE_NUM)
+    windows.mainloop()
