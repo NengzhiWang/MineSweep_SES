f06558ec51fe7f3b7fc004154ef3e7e0bdb8fab0
wnz <wnz_99@hust.edu.cn>
2020-04-08 09:46:45 +0800

    new test case for auto play

--- unit_test.py
+++ unit_test.py
@@ -1,19 +1,26 @@
+import copy
 import random
 import sys
-import tkinter as tk
 import unittest
 
-from Mine_GUI import Mine_GUI
 from Mine_Map import Mine_Map
 
 # Adjust recursive tree limits
 # default is 1000?
 sys.setrecursionlimit(10000)
+'''
+    unittest for Class Mine_Map
+    test:
+        win a game
+        loss a game
+        replay a game
+        auto play a game with wrong flags
+'''
 
 
 class Mine_Test(unittest.TestCase):
-    SIZE_X = 128
-    SIZE_Y = 128
+    SIZE_X = 96
+    SIZE_Y = 96
     MINE_NUM = (SIZE_X * SIZE_Y) // 8
 
     # init map and setup mines
@@ -22,10 +29,6 @@ class Mine_Test(unittest.TestCase):
         x = random.randint(0, self.SIZE_X - 1)
         y = random.randint(0, self.SIZE_Y - 1)
         self.Mines.Mines_Setup(x, y)
-
-        self.win = tk.Tk()
-        self.GUI = Mine_GUI(self.win, self.SIZE_X, self.SIZE_Y, self.MINE_NUM)
-        self.GUI.Callback_Left(x, y)
 
     # test win the game, flag each mine
     def test_Win_Game(self):
@@ -72,10 +75,30 @@ class Mine_Test(unittest.TestCase):
         self.assertEqual(Old_status, self.Mines.die)
         self.assertEqual(New_status, self.Mines.alive)
 
-    def test_Auto_Play(self):
-        self.Mines.Auto_Play()
+    def test_Auto_Play_2(self):
+        # Some wrong flags
+        for i in range(self.MINE_NUM):
+            x = random.randint(0, self.SIZE_X - 1)
+            y = random.randint(0, self.SIZE_Y - 1)
+            self.Mines.Flag(x, y)
+
+        # DO NOT USE '=', it's reference not copy
+        new_Mine = copy.deepcopy(self.Mines)
+        # Get operate list from a new object
+        Operate_List = new_Mine.Auto_Play()
+        new_Mine_status = new_Mine.Status
+        # finish operates
+        for op in Operate_List:
+            x = op[0]
+            y = op[1]
+            f = op[2]
+            if f == 1:
+                self.Mines.Click(x, y)
+            elif f == 2:
+                self.Mines.Flag(x, y)
         status = self.Mines.Status
         self.assertEqual(status, self.Mines.win)
+        self.assertEqual(new_Mine_status, new_Mine.win)
 
 
 if __name__ == "__main__":
